f19241c3a73b87896104e040aff4449dc52c7045
Joseph PRIOU <jpriou@e2r6p17.42.fr>
2018-02-23 15:39:24 +0100

    graphics java remastered

--- graphics_java/src/Main.java
+++ graphics_java/src/Main.java
@@ -1,32 +1,20 @@
 package src;
 
+import java.util.Random;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 
 
 public class Main
 {
-	private static String style = "node {" +
-									"	shape: box;" +
-								    "	fill-color: grey;" +
-								    "}" +
-								    "node.isStart {" +
-								    "	fill-color: green;" +
-								    "}" +
-								    "node.isEnd {" +
-								    "	fill-color: red;" +
-								    "}" +
-								    "node.marked {" +
-								    "	fill-color: blue;" +
-								    "}" +
-								    "edge {" +
-								    "	fill-color: black;" +
-								    "}";
+	private static final String style = getStyleSheet();
+	private static final int nb_colors = 10;
 
 	public static void main(String[] args)
 	{		
 		Lemin lem = Parser.loadData();
 		Graph graph = new MultiGraph("Lem_in Grapher");
+
 		graph.setNullAttributesAreErrors(true);
 		graph.addAttribute("ui.quality");
 		graph.addAttribute("ui.antialias");
@@ -37,6 +25,7 @@ public class Main
 			Node n = graph.addNode(rm.id);
 			n.addAttribute("ui.label", rm.id);
 			n.addAttribute("ui.class", "default");
+			n.addAttribute("link_room", rm);
 			if (rm.isStart)
 			{
 				n.addAttribute("ui.class", "isStart");
@@ -54,47 +43,55 @@ public class Main
 				{
 					graph.addEdge(rm.id + "-" + rm2.id, rm.id, rm2.id);
 				}
-				catch(Exception e)
+				catch (Exception e)
 				{
+					System.err.println("SNA : Creating Edge " + rm.id + "-" + rm2.id);
 				}
 			}
 		}
 
 		graph.display(true);
+		sleep(3);
 
-		sleep(2);
-
-		// Turn loop
 		int nb_turn = 0;
 
 		while (true)
 		{
-			boolean end = true;
-
-			for (Node n : graph.getEachNode())
-			{
-				String type = n.getAttribute("ui.class");
-				if ((type.equals("isStart") || type.equals("isEnd")) == false)
-				{
-					n.setAttribute("ui.class", "default");
-				}
-			}
 			nb_turn++;
-			for (Ant an : lem.listAnts)
-			{
-				Room rm = an.listMap.get(nb_turn);
-				if (rm != null)
-				{
-					end = false;
-					Node n = graph.getNode(rm.id);
-					if (n.getAttribute("ui.class").equals("isEnd") == false)
-						n.setAttribute("ui.class", "marked");
-				}
-			}
-			sleep(1);
-			if (end)
+			sleep(2);
+			if (turn_loop(nb_turn, graph, lem))
 				break ;
 		}
+	}
+
+	public static boolean turn_loop(int nb_turn, Graph graph, Lemin lem)
+	{
+		boolean end = true;
+
+		for (Node n : graph.getEachNode())
+		{
+			String type = n.getAttribute("ui.class");
+			if ((type.equals("isStart") || type.equals("isEnd")) == false)
+			{
+				n.setAttribute("ui.class", "default");
+				n.setAttribute("ui.label", ((src.Room)n.getAttribute("link_room")).id);
+			}
+		}
+		for (Ant an : lem.listAnts)
+		{
+			Room rm = an.listMap.get(nb_turn);
+			if (rm != null)
+			{
+				end = false;
+				Node n = graph.getNode(rm.id);
+				if (n.getAttribute("ui.class").equals("isEnd") == false)
+				{
+					n.setAttribute("ui.class", "marked" + ((an.id - 1) % nb_colors));
+					n.setAttribute("ui.label", n.getAttribute("ui.label") + " <=> " + an.id);
+				}
+			}
+		}
+		return (end);
 	}
 
 	public static void sleep(int sec)
@@ -107,5 +104,38 @@ public class Main
 		{
 			e.printStackTrace();
 		}
+	}
+
+	private static String getStyleSheet()
+	{
+		String normalStyle = 
+					"node {" +
+					"	shape: box;" +
+				    "	fill-color: black;" +
+				    "	text-mode: normal;" +
+				    "	text-color: blue;" +
+				    "}" +
+				    "node.isStart {" +
+				    "	fill-color: green;" +
+				    "}" +
+				    "node.isEnd {" +
+				    "	fill-color: red;" +
+				    "}" +
+				    "edge {" +
+				    "	fill-color: black;" +
+				    "}";
+		Random ran = new Random();
+		for (int i = 1; i <= nb_colors; i++)
+		{
+			int r = ran.nextInt(8) * 16 + 4 * 16;
+			int g = ran.nextInt(8) * 16 + 4 * 16;
+			int b = ran.nextInt(8) * 16 + 8 * 16;
+
+			normalStyle +=
+				"node.marked" + i + " {" + 
+				"	fill-color: rgb(" + r + ", " + g + ", " + b + ");" +
+				"}";
+		}
+		return (normalStyle);
 	}
 }
